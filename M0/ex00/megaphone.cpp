2439f50a6b27065b28a4de6b4dc192dd39ad6429
Samy Ben Tayeb <sben-tay@bess-f2r1s8.clusters.42paris.fr>
2025-01-28 19:25:27 +0100

    remove transform and finished ex0

--- M0/ex00/megaphone.cpp
+++ M0/ex00/megaphone.cpp
@@ -6,36 +6,30 @@
 /*   By: sben-tay <sben-tay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 16:43:20 by sben-tay          #+#    #+#             */
-/*   Updated: 2025/01/14 00:14:27 by sben-tay         ###   ########.fr       */
+/*   Updated: 2025/01/28 19:24:07 by sben-tay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
-#include <algorithm>
 #include <cctype>
 
 int main(int argc, char **argv)
 {
-	int		i = 1;
-	std::string str;
-
 	if (argc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	}
 	else
 	{
-		while (argv[i])
+		for (int i = 1; argv[i]; i++)
 		{
-			str = argv[i];
-			std::transform(str.begin(), str.end(), str.begin(), (int(*)(int))std::toupper);
-			if (i < argc - 1)
-			std::cout << str << " ";
-			else
-				std::cout << str << std::endl;
-			i++;
+			for (int j = 0; argv[i][j]; j++)
+			{
+				std::cout << (char)toupper(argv[i][j]);
+			}
 		}
+		std::cout << std::endl;
 	}
 	return (0);
 }
