543d6d03bae5c1282d8e9212c119756191ea96c1
Samy Ben Tayeb <sben-tay@f2r5s14.clusters.42paris.fr>
2025-06-27 18:06:00 +0200

    created folder M9 and ex00

---
+++ M9/ex00/BitcoinExchange.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sben-tay <sben-tay@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/27 18:04:24 by sben-tay          #+#    #+#             */
+/*   Updated: 2025/06/27 18:04:43 by sben-tay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+# include <iostream>
+# include <fstream>
+# include <sstream>
+# include <map>
+# include <string>
+# include <stdexcept>
+
+
+
+#endif
