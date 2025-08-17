e9ca255e40dab4ca1baa2bbc075cdb2ca0d803c7
Samy Ben Tayeb <sben-tay@f2r7s17.clusters.42paris.fr>
2025-08-17 15:53:51 +0200

    new: add switch case

--- M1/ex06/main.cpp
+++ M1/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sben-tay <sben-tay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 16:48:51 by sben-tay          #+#    #+#             */
-/*   Updated: 2025/08/11 10:44:34 by sben-tay         ###   ########.fr       */
+/*   Updated: 2025/08/17 15:52:02 by sben-tay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,41 +18,45 @@ int main( int argc, char **argv )
 	Harl instance;
 
 	if (argc != 2)
+	{
 		std::cerr << "[ Usage: ./harlFilter <level> ]" << std::endl;
-	std::string args = argv[1];
-	if (args == "DEBUG")
-	{
-		instance.complain(args);
-		std::cout << std::endl;
-		instance.complain("INFO");
-		std::cout << std::endl;
-		instance.complain("WARNING");
-		std::cout << std::endl;
-		instance.complain("ERROR");
-	}
-	else if (args == "INFO")
-	{
-		instance.complain(args);
-		std::cout << std::endl;
-		instance.complain("WARNING");
-		std::cout << std::endl;
-		instance.complain("ERROR");
-	}
-	else if (args == "WARNING")
-	{
-		instance.complain(args);
-		std::cout << std::endl;
-		instance.complain("ERROR");
-	}
-	else if (args == "ERROR")
-	{
-		instance.complain(args);
-	}
-	else
-	{
-		std::cerr << "[ Probably complaining about insignificant problems ]" << std::endl;
 		return (1);
 	}
-	std::cout << std::endl;
+
+	std::string level[4] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+	std::string args = argv[1];
+
+	int idx = -1;
+	for(int i = 0; i < 4; i++)
+	{
+		if (args == level[i])
+		{
+			idx = i;
+			break;
+		}
+	}
+
+	switch(idx)
+	{
+		case 0:
+			instance.complain("DEBUG");
+			std::cout << std::endl;
+			//break;
+		case 1:
+			instance.complain("INFO");
+			std::cout << std::endl;
+			//break;
+		case 2:
+			instance.complain("WARNING");
+			std::cout << std::endl;
+			//break;
+		case 3:
+			instance.complain("ERROR");
+			std::cout << std::endl;
+			break;
+		default:
+			std::cerr << "[ Probably complaining about insignificant problems ]" << std::endl;
+			return (1);
+	}
 	return (0);
 }
